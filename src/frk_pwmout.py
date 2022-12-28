f1785b099d3656a144fd4bf854ed5a8f0a366ad7
Nathan Woody <nathan@nathanwoody.com>
2022-12-28 05:40:57 +0000

    Commit to latest

--- src/frk_pwmout.py
+++ src/frk_pwmout.py
@@ -1,20 +1,33 @@
-from framework import Driver
 import pwmio
+import asyncio
 
-class PWMOut(Driver):
-    _defaults = {'duty_cycle': 0,
-                 'frequency': 500,
-                 'variable_frequency': False}
-
+class PWMOut:
+    variable_frequency = False
+    frequency = 500
+    duty_cycle = 0
+    value = 0
+    sleep = 0.1
+    
     def _init_device(self):
         self._device = pwmio.PWMOut(self._pin,
                                     duty_cycle=self._duty_cycle,
                                     frequency=self._frequency,
                                     variable_frequency=self._variable_frequency)
-
+    
+    async def _run(self):
+        _variable_frequency = self._variable_frequency
+        while True:
+            if _variable_frequency != self._variable_frequency:
+                self._init_device()
+                _variable_frequency = self._variable_frequency
+            await asyncio.sleep(self._sleep)
+    
     def _set_frequency(self, v):
         if self._variable_frequency:
-            self._device.frequency = self._frequency = v
-
+            self._frequency = v
+    
     def _set_duty_cycle(self, v):
-        self._device.duty_cycle = self._duty_cycle = v
+        self._device.duty_cycle = self._duty_cycle = self._value = v
+    
+    def _set_value(self, v):
+        self._device.duty_cycle = self._duty_cycle = self._value = v
